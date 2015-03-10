1c9d2aff4e6f9bb2dd6c746dcb2cc89fef60aed1
D Frost <flaunlmail-github@yahoo.co.uk>
2015-03-11 06:02:28 +1300

    Corrected conflicting declaration of process_data() in  src/session.c

--- src/session.c
+++ src/session.c
@@ -107,7 +107,7 @@ const char *session_error_message[] = {
 // cgi.c
 extern int headers_initialized;
 extern void libcgi_error(int error_code, const char *msg, ...);
-extern formvars *process_data(char *query, formvars **start, formvars **last,
+extern formvars *process_data(const char *query, formvars **start, formvars **last,
                               const char sep_value, const char sep_name);
 
 // Error types
