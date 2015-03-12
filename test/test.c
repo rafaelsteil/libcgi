c61940062b2e56ebb18cbcff1da501c6a9c7d9bb
D Frost <flaunlmail-github@yahoo.co.uk>
2015-03-13 05:20:11 +1300

    Started writing test code

---
+++ test/test.c
@@ -0,0 +1,100 @@
+/* test cases for module "cgi.c" */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "cgi.h"
+
+static void
+test_cgi_escape_special_chars(void)
+{
+	const char *esc_valid = "%._-+0123456789"
+	                        "abcdefghijklmnopqrstuvwxyz"
+	                        "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char str[256];
+	char *esc, *unesc;
+	int c;
+
+	for (c = 0; c < 256; ++c)
+		str[c] = (char) c + 1;
+	str[c] = 0;
+
+	assert(strlen(str) == 255);
+
+	assert(esc = cgi_escape_special_chars(str));
+	assert(unesc = cgi_unescape_special_chars(esc));
+	assert(strcmp(esc, unesc));
+	assert(! strcmp(str, unesc));
+	assert(strspn(esc, esc_valid) == strlen(esc));
+}
+
+static void
+test_process_data(void)
+{
+	formvars *fvp, *iter;
+	const char *data[] = {"1=one&second=%20.two-%30&33=three+three",
+	                      "&1=one&second=%20.two-%30&33=three+three",
+	                      "=noname&1=one&&second=%20.two-%30&33=three+three",
+	                      "==&&=&&1=one&second=%20.two-%30&==&33=three+three&=&",
+	                      NULL};
+	int i;
+
+	for (i = 0; data[i]; ++i)
+	{
+		assert(formvars_start == NULL);
+		assert(formvars_last == NULL);
+
+		fvp = process_data(data[i], &formvars_start, &formvars_last, '=', '&');
+
+		assert(formvars_start && formvars_last && fvp);
+		assert(fvp == formvars_start);
+
+		assert(! strcmp(fvp->name, "1"));
+		assert(! strcmp(fvp->value, "one"));
+		assert(iter = fvp->next);
+
+		assert(! strcmp(iter->name, "second"));
+		assert(! strcmp(iter->value, " .two-0"));
+		assert(iter = iter->next);
+
+		assert(! strcmp(iter->name, "33"));
+		assert(! strcmp(iter->value, "three three"));
+
+		assert(iter == formvars_last);
+		assert(! (iter = iter->next));
+
+		cgi_end();
+	}
+}
+
+void
+test_cgi_param_multiple(void)
+{
+	const char *data = "zero=0&one=one&one=two&two=three&one=four&three=3";
+
+	process_data(data, &formvars_start, &formvars_last, '=', '&');
+
+	assert(! strcmp(cgi_param_multiple("one"), "one"));
+	assert(! strcmp(cgi_param_multiple("one"), "two"));
+	assert(! strcmp(cgi_param_multiple("one"), "four"));
+	assert(! cgi_param_multiple("one"));
+
+	cgi_end();
+}
+
+
+/*****************************************************************************/
+
+int
+main(void)
+{
+	test_cgi_escape_special_chars();
+	test_process_data();
+	test_cgi_param_multiple();
+
+	puts("Tests passed.");
+	return 0;
+}
+
