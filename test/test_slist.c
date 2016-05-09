a730ad0830a4129fc8ca721c1b37882d6e2d1bb3
Alexander Dahl <post@lespocky.de>
2016-05-09 10:34:43 +0200

    tests: implement test for slist_add()

--- test/test_slist.c
+++ test/test_slist.c
@@ -9,6 +9,7 @@
  **********************************************************************/
 
 #include <stddef.h>
+#include <string.h>
 
 #include "cgi_test.h"
 
@@ -31,15 +32,38 @@ int main( int argc, char *argv[] )
 
 int add( void )
 {
-	formvars *item = NULL, *start = NULL, *last = NULL;
+	int			i;
+	formvars	*item[2] = { NULL, NULL }, *start = NULL, *last = NULL;
 
-	check( (item = calloc( 1, sizeof(formvars) )) != NULL, "calloc" );
+	check( (item[0] = calloc( 1, sizeof(formvars) )) != NULL, "calloc item 0" );
+	check( (item[0]->name = strdup( "item 0" )) != NULL, "strdup name item 0" );
+	check( (item[0]->value = strdup( "zero" )) != NULL, "strdup value item 0" );
 
-	/*	TODO	add checks and actually test slist_add() !!	*/
+	slist_add( item[0], &start, &last );
+	check( start == item[0], "start == first item" );
+	check( last == item[0], "last == first item" );
+	check( item[0]->next == NULL, "first item next == null" );
+
+	check( (item[1] = calloc( 1, sizeof(formvars) )) != NULL, "calloc item 1" );
+	check( (item[1]->name = strdup( "item 1" )) != NULL, "strdup name item 1" );
+	check( (item[1]->value = strdup( "one" )) != NULL, "strdup value item 1" );
+	slist_add( item[1], &start, &last );
+	check( start == item[0], "start == first item" );
+	check( last == item[1], "last == first item" );
+	check( item[0]->next == item[1], "first item next == item 1" );
+	check( item[1]->next == NULL, "second item next == null" );
+
+	slist_free( &start );
+	check( start == NULL, "slist_free reset start" );
 
 	return EXIT_SUCCESS;
 
 error:
+	for ( i = 0; i < 2; i++ ) {
+		if ( item[i]->name ) free( item[i]->name );
+		if ( item[i]->value ) free( item[i]->value );
+		if ( item[i] ) free( item[i] );
+	}
 	return EXIT_FAILURE;
 }
 
