27a2ec855b1adeec4f8396bde3020859551b0350
Alexander Dahl <post@lespocky.de>
2016-05-09 13:23:30 +0200

    tests: test deleting from zero or one item slist

--- test/test_slist.c
+++ test/test_slist.c
@@ -16,11 +16,15 @@
 #include "cgi.h"
 
 int add( void );
+int delete_from_empty_list( void );
+int delete_from_one_item_list( void );
 
 int main( int argc, char *argv[] )
 {
 	struct cgi_test_action	actions[] = {
-		{ "add",	add	},
+		{ "add",		add							},
+		{ "deletezero",	delete_from_empty_list		},
+		{ "deleteone",	delete_from_one_item_list	},
 	};
 
 	/*	require at least one argument to select test	*/
@@ -53,6 +57,8 @@ int add( void )
 	check( item[0]->next == item[1], "first item next == item 1" );
 	check( item[1]->next == NULL, "second item next == null" );
 
+	/*	TODO	add tests for use with wrong arguments!	*/
+
 	slist_free( &start );
 	check( start == NULL, "slist_free reset start" );
 
@@ -64,6 +70,61 @@ error:
 		if ( item[i]->value ) free( item[i]->value );
 		if ( item[i] ) free( item[i] );
 	}
+	return EXIT_FAILURE;
+}
+
+int delete_from_empty_list( void )
+{
+	formvars	*start = NULL, *last = NULL;
+
+	/*	add no item, try to delete an item	*/
+	check( slist_delete( "not in there", &start, &last ) == 0, "not found" );
+
+	return EXIT_SUCCESS;
+
+error:
+	return EXIT_FAILURE;
+}
+
+int delete_from_one_item_list( void )
+{
+	const char	item_name[] = "item";
+	formvars	*item, *start = NULL, *last = NULL;
+
+	/*	add one item, delete one item	*/
+	check( (item = calloc( 1, sizeof(formvars) )) != NULL, "calloc item" );
+	check( (item->name = strdup( item_name )) != NULL, "strdup name item" );
+	check( (item->value = strdup( "single" )) != NULL, "strdup value item" );
+	slist_add( item, &start, &last );
+
+	/*	try deleting not existing item and check list does not change	*/
+	check( slist_delete( "not in there", &start, &last ) == 0, "not found" );
+	check( start == item, "start == first item" );
+	check( last == item, "last == first item" );
+	check( item->next == NULL, "first item next == null" );
+
+	/*	try deleting our item
+	 *
+	 *	NOTE	libcgi does not free the memory of deleted items	*/
+	check( slist_delete( item_name, &start, &last ) == 1, "found" );
+	check( start == NULL, "start = null" );
+	check( last == NULL, "last = null" );
+
+	free( item->name );
+	free( item->value );
+	free( item );
+	item = NULL;
+
+	/*	the list should already be empty	*/
+	slist_free( &start );
+	check( start == NULL, "slist_free reset start" );
+
+	return EXIT_SUCCESS;
+
+error:
+	if ( item->name ) free( item->name );
+	if ( item->value ) free( item->value );
+	if ( item ) free( item );
 	return EXIT_FAILURE;
 }
 
