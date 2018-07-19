rafaelsteil/libcgi

commit 0643151cf9ee8e09f4a613256bada8cde24b75c4

cmake: introduce cmake as alternative build system
see #27

Signed-off-by: Alexander Dahl <post@lespocky.de>

src/CMakeLists.txt
@@ -1,0 +1,50 @@
+#
+# Copyright 2013,2016 Alexander Dahl <post@lespocky.de>
+#
+configure_file(
+	"${CMAKE_CURRENT_SOURCE_DIR}/config.h.cmake.in"
+	"${CMAKE_CURRENT_BINARY_DIR}/config.h"
+)
+
+include_directories(
+	"${CMAKE_CURRENT_BINARY_DIR}"
+)
+
+set(CGI_SRC
+	base64.c
+	cgi.c
+	cookie.c
+	error.c
+	general.c
+	list.c
+	#	md5.c
+	session.c
+	string.c
+)
+
+# create binary
+add_library(${PROJECT_NAME}-shared SHARED ${CGI_SRC})
+set_target_properties(${PROJECT_NAME}-shared PROPERTIES
+	OUTPUT_NAME	${PROJECT_NAME}
+	SOVERSION	${PACKAGE_MAJOR_VERSION}
+	VERSION		${PACKAGE_VERSION}
+)
+
+# install binary
+install(TARGETS ${PROJECT_NAME}-shared
+	EXPORT ${PROJECT_NAME}-targets
+	LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+)
+
+# install cmake targets
+install(EXPORT ${PROJECT_NAME}-targets
+	DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+)
+
+# install headers
+install(FILES
+	cgi.h
+	error.h
+	session.h
+	DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${PROJECT_NAME}"
+)

src/config.h.cmake.in
@@ -1,0 +1,1 @@
+#define	HAVE_MD5	0

CMakeLists.txt
@@ -1,0 +1,46 @@
+#
+# Copyright 2013,2016 Alexander Dahl <post@lespocky.de>
+#
+cmake_minimum_required(VERSION 2.8.8)
+
+project(cgi)
+string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UC)
+string(TOLOWER ${PROJECT_NAME} PROJECT_NAME_LC)
+set(LIBPREFIX ${PROJECT_NAME})
+set(LIBPREFIX_LC ${PROJECT_NAME_LC})
+set(LIBPREFIX_UC ${PROJECT_NAME_UC})
+
+set(PACKAGE_MAJOR_VERSION "1")
+set(PACKAGE_MINOR_VERSION "0")
+set(PACKAGE_BUILD_VERSION "0")
+set(PACKAGE_VERSION "${PACKAGE_MAJOR_VERSION}.${PACKAGE_MINOR_VERSION}.${PACKAGE_BUILD_VERSION}")
+
+# includes
+include(CMakePackageConfigHelpers)	# cmake 2.8.8
+include(FeatureSummary)
+include(GNUInstallDirs)				# cmake 2.8.5
+
+# subdirectories
+add_subdirectory("src")
+
+# cmake package stuff
+configure_package_config_file(${PROJECT_NAME_LC}-config.cmake.in
+	"${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME_LC}-config.cmake"
+	INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+	PATH_VARS CMAKE_INSTALL_INCLUDEDIR
+	NO_CHECK_REQUIRED_COMPONENTS_MACRO
+)
+write_basic_package_version_file(
+	"${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME_LC}-config-version.cmake"
+	VERSION ${PACKAGE_VERSION}
+	COMPATIBILITY AnyNewerVersion
+)
+install(FILES
+	"${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME_LC}-config.cmake"
+	"${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME_LC}-config-version.cmake"
+	DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+)
+
+# feature summary
+message(STATUS "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
+feature_summary(WHAT ALL)

.gitignore
@@ -9,3 +9,4 @@ libcgi.so
 autom4te.cache
 configure
 src/config.h
+*swp

cgi-config.cmake.in
@@ -1,0 +1,14 @@
+#
+# Copyright 2013 Alexander Dahl <post@lespocky.de>
+#
+
+# paths
+@PACKAGE_INIT@
+set_and_check(CGI_INCLUDE_DIRS "@PACKAGE_CMAKE_INSTALL_INCLUDEDIR@/@PROJECT_NAME@")
+
+# targets
+get_filename_component(_dir "${CMAKE_CURRENT_LIST_FILE}" PATH)
+if(NOT TARGET @PROJECT_NAME@-shared)
+	include("${_dir}/@PROJECT_NAME@-targets.cmake")
+endif(NOT TARGET @PROJECT_NAME@-shared)
+set(CGI_LIBRARIES @PROJECT_NAME@-shared)

commit b4a284c5ff08e93be3b496361a66051f576e26b6

Mark function md5() as deprecated and compile md5.c again
Quote from issue #15:

> libcgi contains an implementation of the md5 hash function, however
> its not used inside the library itself and the status regarding
> license or potential security issues (despite the weekness of md5
> itself) is not clear. If a user needs md5 he or she could get it from
> a specialized library.

Although md5 is deprecated (#15) we can not simply remove the code from
the API. Do that later.

The HAVE_MD5 macro is useless, that was a relict from the old autotools
build and actually meant »build with md5«, so we can simply drop the
build time generated file and that macro.

Fixes: 0643151cf9ee8e09f4a613256bada8cde24b75c4
Signed-off-by: Alexander Dahl <post@lespocky.de>

src/CMakeLists.txt
@@ -2,11 +2,6 @@
 # Copyright 2013,2016,2018 Alexander Dahl <post@lespocky.de>
 #
 
-configure_file(
-	"${CMAKE_CURRENT_SOURCE_DIR}/config.h.cmake.in"
-	"${CMAKE_CURRENT_BINARY_DIR}/config.h"
-)
-
 set(CGI_SRC
 	base64.c
 	cgi.c
@@ -14,7 +9,7 @@ set(CGI_SRC
 	error.c
 	general.c
 	list.c
-	#	md5.c
+	md5.c
 	session.c
 	string.c
 )

src/cgi.h
@@ -23,10 +23,20 @@
 
 #include <stdio.h>
 
+#if defined(__GNUC__)
+#define CGI_DEPRECATED __attribute__ ((deprecated))
+#elif defined(_MSC_VER)
+#define CGI_DEPRECATED __declspec(deprecated)
+#elif defined(__clang__)
+#define CGI_DEPRECATED __deprecated
+#else
+#pragma message("WARNING: You need to implement CGI_DEPRECATED for this compiler")
+#define CGI_DEPRECATED
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 // general purpose linked list. Actually isn't very portable
 // because uses only 'name' and 'value' variables to store data.
@@ -81,7 +91,7 @@ extern char *stripslashes(char *str);
 extern char *str_base64_encode(char *str);
 extern char *str_base64_decode(char *str);
 extern char *recvline(FILE *fp);
-extern char *md5(const char *str);
+CGI_DEPRECATED char *md5(const char *str);
 extern char *cgi_ltrim(char *str);
 extern char *cgi_rtrim(char *str);
 extern char *cgi_trim(char *str);

src/config.h.cmake.in
@@ -1,1 +1,0 @@
-#define	HAVE_MD5	0

src/md5.c
@@ -23,15 +23,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "config.h"
 #include "error.h"
 #include "cgi.h"
 
 /** @ingroup libcgi_general
 * @{
 */
-
-#if HAVE_MD5
 
 #ifndef MD5_H
 #define MD5_H
@@ -95,7 +92,6 @@ char *md5(const char *str)
 	// returning a encrypted string
 	return tmp;
 }
-#endif
 
 /*=======================================================================*/
 